092f4e725a71e5c68a52f385ea6907638e98a856
kajjfasz9 <143551667+kajjfasz9@users.noreply.github.com>
2023-09-03 18:00:01 -0400

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,79 @@
+package com.grocerymanagement.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	@Column(name = "last_name")
+	private String LastName;
+	@Column(name = "department")
+	private String department;
+	@Column(name = "position")
+	private String position;
+	@Column(name = "wage")
+	private Integer wage;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName, String department, String position, Integer wage) {
+		super();
+		this.firstName = firstName;
+		LastName = lastName;
+		this.department = department;
+		this.position = position;
+		this.wage = wage;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public Integer getWage() {
+		return wage;
+	}
+	public void setWage(Integer wage) {
+		this.wage = wage;
+	}
+
+}
